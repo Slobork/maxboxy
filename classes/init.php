fc61bc50f00bb37841f746be2101e46043c3a10b
Slobodan <sloooba@yahoo.com>
2023-04-28 16:39:25 +0200

    Version change

--- classes/init.php
+++ classes/init.php
@@ -46,7 +46,7 @@ if (! class_exists('Max_Boxy')) {
          */
         public static function version()
         {
-            $plugin_version = '1.0.8';
+            $plugin_version = '1.0.9';
             return $plugin_version;
         }
 

28aee8015becdb39d618ec136d577cf5bd076dfd
Slobodan <sloooba@yahoo.com>
2024-01-06 11:40:22 +0100

    Version bump

--- maxboxy.php
+++ maxboxy.php
@@ -15,7 +15,7 @@
  * Author URI:          https://maxpressy.com
  * License:             GPL v2 or later
  * License URI:         https://www.gnu.org/licenses/gpl-2.0.html
- * Version:             1.1.3
+ * Version:             1.1.4
  * Text Domain:         maxboxy
  * Domain Path:         /languages
  * Requires at least:   5.8

abc5f1a38682222c0ff79c03d03bd18b89fd03d5
Slobodan <sloooba@yahoo.com>
2025-03-25 11:54:35 +0100

    Bump the version

--- maxboxy.php
+++ maxboxy.php
@@ -3,7 +3,7 @@
  * Plugin Name:         MaxBoxy
  * Description:         Make Conversion Boxes, Popups, Floats and Inject Any Content in a WorsPress way!
  *
- * PHP version  7.3.5
+ * PHP version  7.4
  *
  * @category Conversion
  * @package  MaxBoxy
@@ -15,10 +15,10 @@
  * Author URI:          https://maxpressy.com
  * License:             GPL v2 or later
  * License URI:         https://www.gnu.org/licenses/gpl-2.0.html
- * Version:             1.1.9
+ * Version:             1.2.0
  * Text Domain:         maxboxy
  * Domain Path:         /languages
- * Requires at least:   5.8
+ * Requires at least:   6.7
  */
 
 if (! defined('ABSPATH') ) {
